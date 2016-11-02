ab44c68f935a33eb399cfe5957b772f18880bf5a
zhaopei <zhaopei0418@aol.com>
2016-11-02 19:09:29 +0800

    修改页面添加跳转页面

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,8 +1,28 @@
-function goToPageNum(formId, pageNum) {
+$(function() {
+	$(".input-date").datetimepicker({
+		locale: 'zh-CN',
+		format: 'YYYY-MM-DD'
+	});
+
+	$(document).keydown(function(event) {
+		if (13 == event.keyCode) {
+			$(".page-num").val("1");
+			$(".search-form").submit();
+		}
+
+		if (27 == event.keyCode) {
+			resetForm();
+		}
+	});
+});
+
+function goToPageNum(formId, pageNum, maxPageNum) {
+	if (0 >= pageNum || pageNum > maxPageNum) {
+		alert("请输入合理的页数!");
+		return;
+	}
 	var formObj = $("#" + formId);
-	var param = "<input type='hidden' name='pageNum' value='" + pageNum
-			+ "' />";
-	formObj.append(param);
+	$(".page-num").val(pageNum + "");
 	formObj.submit();
 }
 
@@ -10,9 +30,15 @@ function logout() {
     $("#form_logout").submit();
 }
 
+function formSubmit(obj) {
+	$(".page-num").val("1");
+	$(obj).parents("form").submit();
+}
+
 function addFormParaQuery(formId, name, value) {
 	var formObj = $("#" + formId);
 	var inputObj = formObj.find("[name='" + name + "']");
+	$(".page-num").val("1");
 	var param = "";
 	if (0 == inputObj.length) {
 		var param = "<input type='hidden' name='" + name + "' value='" 
@@ -40,7 +66,12 @@ function switchActiveMenu(menuId) {
 }
 
 function resetForm(formId) {
-	var formObj = $("#" + formId);
+	var formObj;
+	if (undefined == formId) {
+		formObj = $(".search-form");
+	} else {
+		formObj = $("#" + formId);
+	}
 	formObj.find("input").val("");
 	formObj.find("select").val("");
 }
