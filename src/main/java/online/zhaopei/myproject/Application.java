006d47123cb304db39c179adf97e2cfaab0bae83
zhaopei <zhaopei0418@aol.com>
2016-12-09 18:55:09 +0800

    springboot修改致1.4.2版本,apidocs/下面的文件不用授权就可以访问

--- src/main/java/online/zhaopei/myproject/Application.java
+++ src/main/java/online/zhaopei/myproject/Application.java
@@ -31,7 +31,7 @@ public class Application extends SpringBootServletInitializer {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
 					.antMatchers("/js/**", "/css/**", "/fonts/**", "/images/**", "/vendors/**", "/locales/**",
-							"/favicon.ico", "/api/**")
+							"/favicon.ico", "/api/**", "/apidocs/**")
 					.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
 					.logout().permitAll();
 		}
